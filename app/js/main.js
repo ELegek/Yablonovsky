ce58662dc1d149b427939ba8310d759748da2607
Evgeny <evgeny.legek@yandex.ru>
2021-09-08 15:03:07 +0300

    start accordeon

--- app/js/main.js
+++ app/js/main.js
@@ -1,100 +1,128 @@
-'use strict'
+window.addEventListener('DOMContentLoaded', () => {
+    ' use strict ';
 
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows()
-        );
-    },
-};
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows()
+            );
+        },
+    };
 
-if (isMobile.any()) {
-    document.body.classList.add('_touch');
+    if (isMobile.any()) {
+        document.body.classList.add('_touch');
 
-    let menuArrows = document.querySelectorAll('.menu__arrow');
-    if (menuArrows.length > 0) {
-        for (let i = 0; i < menuArrows.length; i++) {
-            const menuArrow = menuArrows[i];
-            menuArrow.addEventListener('click', function () {
-                menuArrow.parentElement.classList.toggle('_active');
-            });
-        }
-    }
-
-} else {
-    document.body.classList.add('_pc');
-}
-
-// Меню бургер
-
-const headerMenu = document.querySelector('.header__burger');
-const iconMenu = document.querySelector('.header__burger-btn')
-const menuBody = document.querySelector('.menu__body');
-if (iconMenu) {
-    headerMenu.addEventListener('click', function (e) {
-        document.body.classList.toggle('_lock')
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
-}
-
-// Прокрута при клике
-
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', onMenuLinkClick);
-    });
-
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-            if (iconMenu.classList.contains('_active')) {
-                document.body.classList.remove('_lock');
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
+        let menuArrows = document.querySelectorAll('.menu__arrow');
+        if (menuArrows.length > 0) {
+            for (let i = 0; i < menuArrows.length; i++) {
+                const menuArrow = menuArrows[i];
+                menuArrow.addEventListener('click', function () {
+                    menuArrow.parentElement.classList.toggle('_active');
+                });
             }
+        }
 
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: 'smooth'
-            });
-            e.preventDefault();
+    } else {
+        document.body.classList.add('_pc');
+    }
+
+    // Меню бургер
+
+    const headerMenu = document.querySelector('.header__burger');
+    const iconMenu = document.querySelector('.header__burger-btn')
+    const menuBody = document.querySelector('.menu__body');
+    if (iconMenu) {
+        headerMenu.addEventListener('click', function (e) {
+            document.body.classList.toggle('_lock')
+            iconMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+        });
+    }
+
+    // Прокрута при клике
+
+
+
+    // Swiper
+
+    const swiper = new Swiper('.swiper', {
+
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            // dynamicBullets: true,
+        }
+
+    });
+
+    // Tabs
+
+    let tab = document.querySelectorAll('.description-header-tab');
+    let info = document.querySelector('.description-header');
+    let tabContent = document.querySelectorAll('.description-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
         }
     }
-}
 
-// Swiper
-
-const swiper = new Swiper('.swiper', {
+    hideTabContent(1);
 
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        // dynamicBullets: true,
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
     }
+
+
+    info.addEventListener('click', (e) => {
+        e.preventDefault()
+        let target = e.target;
+        if (target && target.classList.contains('description-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+
+    // Accordeon
+
+    document.querySelectorAll('.accordeon-trigger').forEach((item) => {
+        item.addEventListener('click', () => {
+            const parent = item.parentNode;
+
+            parent.classList.toggle('accordion-item--active');
+        })
+    });
+
 
 });
